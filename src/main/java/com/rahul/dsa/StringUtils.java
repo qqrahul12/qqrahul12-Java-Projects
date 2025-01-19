c90ea09b44a5d0eb65a79ce9327f49ffa2a36879
rjain1 <rjain1@cvent.com>
2025-01-19 18:02:27 +0530

    Added solution for leetcode #394

--- src/main/java/com/rahul/dsa/StringUtils.java
+++ src/main/java/com/rahul/dsa/StringUtils.java
@@ -2,6 +2,8 @@ package com.rahul.dsa;
 
 import com.rahul.password.util;
 
+import java.util.Stack;
+
 public class StringUtils {
     public static boolean areAllUpperCase(String s) {
         return s.chars().allMatch(Character::isUpperCase);
@@ -40,6 +42,62 @@ public class StringUtils {
             return -1;
         }
         return s.indexOf(sub);
+    }
+
+    public static String repeat(String a, int count) {
+        return String.valueOf(a).repeat(Math.max(0, count));
+    }
+
+    // https://leetcode.com/problems/decode-string/
+    public static String decodeString(String s) {
+        if(s.length()==1) return s;
+        int i = 0;
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        while(i < n) {
+            char c = s.charAt(i);
+            if(Character.isDigit(c)) {
+                int y = 0;
+                while(Character.isDigit(s.charAt(i))) {
+                    int x = s.charAt(i) - '0';
+                    y = y * 10 + x ;
+                    i++;
+                }
+                i = i + 1;
+                Stack<Character> st = new Stack<>();
+                StringBuilder rs = new StringBuilder();
+                st.push('[');
+                st.push(s.charAt(i));
+                rs.append(s.charAt(i));
+                i++;
+                while(i<n && !st.empty()) {
+                    if(s.charAt(i) == ']') {
+                        char z = st.peek();
+                        while(!st.empty() && z != '[') {
+                            st.pop();
+                            if(!st.empty())
+                                z = st.peek();
+                        }
+                        if(z=='[') st.pop();
+                        i++;
+                        rs.append(']');
+                    } else {
+                        st.push(s.charAt(i));
+                        rs.append(s.charAt(i));
+                        i++;
+                    }
+                }
+                System.out.println(rs);
+                String z = decodeString(rs.toString());
+                sb.append(repeat(z, y));
+            } else if(s.charAt(i) != ']') {
+                sb.append(c);
+                i++;
+            } else {
+                i++;
+            }
+        }
+        return sb.toString();
     }
 
 }
