471294885f69852005366346a47fc6534c65b7f1
qqrahul12 <qqrahul12@outlook.com>
2025-05-12 19:51:51 +0530

    Update pom to use next ssr features.

--- SplitMoney/src/ui/app/viewGroups/page.tsx
+++ SplitMoney/src/ui/app/viewGroups/page.tsx
@@ -4,12 +4,7 @@ export default async function Groups() {
     const res = await fetch("http://localhost:8080/groups", { cache: "no-store" });
     const groups = await res.json();
 
-    // Handler for group selection
-    const handleGroupSelect = (group: any) => {
-        console.log("Selected group:", group);
-    };
-
     return (
-        <AllGroupsList groups={groups} onGroupSelect={handleGroupSelect} />
+        <AllGroupsList groups={groups} />
     );
 }
